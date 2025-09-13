10f4af53ade9aa78ebd9a05fa649ec17de7721e5
Koo <-->
2025-09-13 17:20:06 +0900

    initial commit

---
+++ src/main/java/com/io/echo/EchoController.java
@@ -0,0 +1,15 @@
+package com.io.echo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class EchoController {
+
+  @GetMapping
+  public String echo() {
+    return "Hello, World!";
+  }
+}
