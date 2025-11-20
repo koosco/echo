ba5fa4522269c1fa584fedca71d1b13a022740fd
Koo <koosco136@gmail.com>
2025-11-21 08:20:34 +0900

    add log in /api endpoint

--- src/main/java/com/io/echo/EchoController.java
+++ src/main/java/com/io/echo/EchoController.java
@@ -1,5 +1,7 @@
 package com.io.echo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class EchoController {
 
+  private static final Logger logger = LoggerFactory.getLogger(EchoController.class);
+
   @GetMapping
   public String echo() {
+    logger.info("Echo API request received");
     return "Hello, World!";
   }
 }
