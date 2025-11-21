13dc9258173bd66645a17e9bb5af51e74c8cbc03
Koo <koosco136@gmail.com>
2025-11-21 16:27:20 +0900

    add greeting.secret to EchoController and application.properties

--- src/main/java/com/io/echo/EchoController.java
+++ src/main/java/com/io/echo/EchoController.java
@@ -1,8 +1,11 @@
 package com.io.echo;
 
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +17,9 @@ public class EchoController {
   @Value("${greeting.message}")
   private String message;
 
+  @Value("${greeting.secret}")
+  private String secret;
+
   private static final Logger logger = LoggerFactory.getLogger(EchoController.class);
 
   @GetMapping
@@ -23,8 +29,10 @@ public class EchoController {
   }
 
   @GetMapping("/welcome")
-  public String welcome() {
+  public ResponseEntity<Map<String, String>> welcome() {
     logger.info("Welcome API request received");
-    return "The message is " + message;
+    return ResponseEntity.ok(Map.of(
+        "message", message,
+        "secret", secret));
   }
 }
