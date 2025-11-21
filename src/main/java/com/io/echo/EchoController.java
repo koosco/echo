3b71ac69d29ebd1f83262ad6ee807e380e6b228e
Koo <koosco136@gmail.com>
2025-11-21 15:07:04 +0900

    add greeting api

--- src/main/java/com/io/echo/EchoController.java
+++ src/main/java/com/io/echo/EchoController.java
@@ -2,6 +2,7 @@ package com.io.echo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class EchoController {
 
+  @Value("${greeting.message}")
+  private String message;
+
   private static final Logger logger = LoggerFactory.getLogger(EchoController.class);
 
   @GetMapping
   public String echo() {
     logger.info("Echo API request received");
-    return "Hello, World!";
+    return "Hello, World!!";
+  }
+
+  @GetMapping("/welcome")
+  public String welcome() {
+    logger.info("Welcome API request received");
+    return "The message is " + message;
   }
 }
